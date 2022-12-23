7a7aee88a905d0114e88c0519ec6e1057a026112
Sergei Miroshnikov <s.miroshnikov@outlook.com>
2022-12-23 22:04:34 +0400

    updated sedawk.sh script

--- sedawk.sh
+++ sedawk.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-# Put you code here
+# a. Create copy of passwd file to passwd_new.
+cp ./passwd ./passwd_new
+file="./passwd_new"
+
+# b. Change shell for user saned from /usr/sbin/nologin to /bin/bash using AWK
+awk -i inplace -F':' -OFS':' '/saned/{gsub("/usr/sbin/nologin","/bin/bash") }; {print}' "${file}"
+
+# c. Change shell for user avahi from /usr/sbin/nologin to /bin/bash using SED
+sed -i '/avahi/s/\/usr\/sbin\/nologin/\/bin\/bash/' "${file}"
+
+# d. Save only 1-st 3-th 5-th 7-th columns of each string based on : delimiter
+awk -i inplace -F':' -v OFS=: '{print $1, $3, $5, $7}' "${file}"
+
+# e. Remove all lines from file containing word daemon
+sed -i '/daemon/d' "${file}"
+
+# f. Change shell for all users with even UID to /bin/zsh
+awk -i inplace -F':' -v OFS=: '{ if ($2%2=="0") {gsub($4,"/bin/zsh") }; {print} }' "${file}"
