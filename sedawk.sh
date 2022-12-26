bd0ead6074185ba2aa0245632fff3ffde3834fa9
Sergei Miroshnikov <s.miroshnikov@outlook.com>
2022-12-26 15:11:44 +0400

    changed 'awk' tasks

--- sedawk.sh
+++ sedawk.sh
@@ -2,19 +2,18 @@
 
 # a. Create copy of passwd file to passwd_new.
 cp ./passwd ./passwd_new
-file="./passwd_new"
 
 # b. Change shell for user saned from /usr/sbin/nologin to /bin/bash using AWK
-awk -i inplace -F':' -OFS':' '/saned/{gsub("/usr/sbin/nologin","/bin/bash") }; {print}' "${file}"
+awk -F: '/saned/{$NF="/bin/bash";}1' OFS=: ./passwd_new > ./tmpfile1 && mv -f ./tmpfile1 ./passwd_new
 
 # c. Change shell for user avahi from /usr/sbin/nologin to /bin/bash using SED
-sed -i '/avahi:/s/\/usr\/sbin\/nologin/\/bin\/bash/' "${file}"
+sed -i '/avahi:/s/\/usr\/sbin\/nologin/\/bin\/bash/' ./passwd_new
 
 # d. Save only 1-st 3-th 5-th 7-th columns of each string based on : delimiter
-awk -i inplace -F':' -v OFS=: '{print $1, $3, $5, $7}' "${file}"
+ awk -F: '{print $1, $3, $5, $7}' OFS=: ./passwd_new > ./tmpfile2 && mv -f ./tmpfile2 ./passwd_new
 
 # e. Remove all lines from file containing word daemon
-sed -i '/daemon/d' "${file}"
+sed -i '/daemon/d' ./passwd_new
 
 # f. Change shell for all users with even UID to /bin/zsh
-awk -i inplace -F':' -v OFS=: '{ if ($2%2=="0") {gsub($4,"/bin/zsh") }; {print} }' "${file}"
+awk -F: '{ if ($3%2=="0") {$NF="/bin/zsh";} }1' OFS=: ./passwd_new > ./tmpfile3 && mv -f ./tmpfile3 ./passwd_new
